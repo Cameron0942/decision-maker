2e1f532deb449a66dccc54e0733805c99422c9b4
Cameron B <Cameron0942@users.noreply.github.com>
2023-09-07 23:11:20 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/pages/decision-new/DecisionNew.scss";`, // Replace with the correct path to your SCSS file
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@import "src/pages/decision-new/DecisionNew.scss";`, // Replace with the correct path to your SCSS file
+  //     },
+  //   },
+  // },
   optimizeDeps: {
     include: ['axios'],
   },
