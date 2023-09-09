9e33d259f3a000e1b34596ef0b2e58ea73516546
Cameron B <Cameron0942@users.noreply.github.com>
2023-09-08 17:18:31 -0700

    used chatGPT to fix '__dirname is not defined in ES module scope' error

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,17 @@ dotenv.config();
 
 import express, { json } from "express";
 import cors from "cors";
-const app = express();
 import path from 'path';
+
+//? EXPRESS
+import { fileURLToPath } from 'url';
+const app = express();
+
+// Get the directory path using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, 'dist')));
 
 //? MONGODB
 import mongoose from "mongoose";
@@ -23,9 +32,6 @@ import getRouter from "./routers/getRouter.js";
 
 app.use(json());
 app.use(cors());
-
-// eslint-disable-next-line no-undef
-app.use(express.static(path.join(__dirname, 'build')));
 
 // eslint-disable-next-line no-undef
 const mongodbURI = process.env.MONGODB_URI;
