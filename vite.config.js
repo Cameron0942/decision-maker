8dc9b0aee808bf076999461449d6ef36895806bf
Cameron B <Cameron0942@users.noreply.github.com>
2023-09-09 22:49:30 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/decision-maker/',
+  base: './',
   plugins: [react()],
   // css: {
   //   preprocessorOptions: {
