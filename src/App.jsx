45f6dac85cf774d2d8db97f7da35ceef8eed6374
Cameron B <Cameron0942@users.noreply.github.com>
2023-09-09 23:19:38 -0700

    repalced browser router with hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //? REACT
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 //? SASS
 import './styles/main.scss';
